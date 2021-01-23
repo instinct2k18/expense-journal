8ce1a58d4683f5ec63a988f88b42ea6578cb5c55
Pavan Hegde <pavanrkadave@gmail.com>
2021-01-23 16:49:29 +0530

    Remove unwanted css class

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div>
         <Nav />
         <Switch>
           <Route path="/" exact component={Home} />
